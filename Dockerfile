279497903df7975f294dff0594d27a9ec80b5813
Natalia Poyarkova <n.poyarkova@yadro.com>
2018-03-26 21:04:50 +0300

    + building app via docker-compose

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.6
-
-RUN git clone https://github.com/Salldor/JumbleRumble.git
-WORKDIR ./JumbleRumble
-RUN pip install -r requirements
-
-EXPOSE 80
-
-ENTRYPOINT ["gunicorn", "-b 127.0.0.1:80","--pythonpath='$PWD'", "-w", "2", "app:app"]
--- web/app.py
+++ web/app.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from werkzeug.contrib.fixers import ProxyFix
-
+#from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-app.wsgi_app = ProxyFix(app.wsgi_app)
+
+#app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
